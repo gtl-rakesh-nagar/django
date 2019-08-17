d424b846a67498a03339aa6b1d7d6fc662c2a074
Rakesh Nagar <rakesh.nagar@thegatewaycorp.com>
2019-08-17 23:04:24 +0530

    Django poll app - more views, render, raising error, templete, url namespacing

--- poll/poll/urls.py
+++ poll/poll/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('poll/', include('pollapp.urls')),
+    path('pollapp/', include('pollapp.urls')),
 ]
