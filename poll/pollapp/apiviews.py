474358be79c08d1eb22d625092a61e1e8c661e46
Rakesh Nagar <rakesh.nagar@thegatewaycorp.com>
2019-08-18 21:10:47 +0530

    Django poll app API - DRF api_view, serializer

--- poll/pollapp/apiviews.py
+++ poll/pollapp/apiviews.py
@@ -1,18 +1,21 @@
-from datetime import datetime
-
 from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 from .models import Question
+from .serializers import QuestionSerializer
 
-@csrf_exempt
+
+@api_view(['GET', 'POST'])
 def questions_view(request):
     if request.method == 'GET':
         return HttpResponse("Not Implemented")
     elif request.method == 'POST':
-        if 'question_text' not in request.POST or 'pub_date' not in request.POST:
-            return HttpResponse("question_text or pub_date missing", status=400)
-        question_text = request.POST['question_text']
-        pub_date = datetime.strptime(request.POST['pub_date'], '%Y-%m-%d')
-        Question.objects.create(question_text=question_text, pub_date=pub_date)
-        return HttpResponse("Question created", status=201)
+        serializer = QuestionSerializer(data=request.data)
+        if serializer.is_valid():
+            question_text = serializer.data['question_text']
+            pub_date = serializer.data['pub_date']
+            Question.objects.create(question_text=question_text, pub_date=pub_date)
+            return Response("Question created", status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
