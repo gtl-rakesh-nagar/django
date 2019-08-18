eb39b882276657e6bddf5b6d5fb262a1d308fb9c
Rakesh Nagar <rakesh.nagar@thegatewaycorp.com>
2019-08-18 22:48:13 +0530

    Django poll app API - Get question

--- poll/pollapp/apiviews.py
+++ poll/pollapp/apiviews.py
@@ -1,3 +1,5 @@
+import json
+
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -10,7 +12,12 @@ from .serializers import QuestionSerializer
 @api_view(['GET', 'POST'])
 def questions_view(request):
     if request.method == 'GET':
-        return HttpResponse("Not Implemented")
+        questions = []
+        for question in Question.objects.all():
+            question_representation = {'question_text': question.question_text,
+                                       'pub_date': question.pub_date.strftime("%Y-%m-%d")}
+            questions.append(question_representation)
+        return HttpResponse(json.dumps(questions), content_type='application/json')
     elif request.method == 'POST':
         serializer = QuestionSerializer(data=request.data)
         if serializer.is_valid():
